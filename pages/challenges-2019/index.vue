390a2f3fcee8132a56f986ae8810c18e9c2e79b3
Lee Ralls <lee@idxbroker.com>
2019-04-17 18:09:25 -0700

    Added remaining youtube hosted videos, removed a challenge and brought back cafe yumm as a sponsor.

--- pages/challenges-2019/index.vue
+++ pages/challenges-2019/index.vue
@@ -13,7 +13,19 @@
       <div v-for="(challenge, key) in challenges" :key="key" class="h4c-challenges">
         <div class="h4c-bold h4c-challenge--title" v-html="challenge.title"/>
         <div v-if="challenge.video" class="h4c-fb-video">
-          <iframe :src="challenge.video" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"/>
+          <iframe v-if="typeof challenge.video === 'string'" :src="challenge.video" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"/>
+          <iframe
+            v-for="(v, k) in challenge.video"
+            v-else-if="typeof challenge.video === 'object'"
+            :key="k"
+            :src="v"
+            width="560"
+            height="315"
+            style="border:none;overflow:hidden"
+            scrolling="no"
+            frameborder="0"
+            allowTransparency="true"
+            allowFullScreen="true"/>
         </div>
         <p v-html="challenge.description"/>
       </div>
@@ -41,22 +53,17 @@ export default {
                 title: "Oregon Campaign Trail: Civic engagement for candidates",
                 description:
                     "Creating a realistic game that would show people interested in running for office the challenges and opportunities they will encounter.",
-                video: ""
+                video: "https://www.youtube.com/embed/0XMC8ReWTRI"
             },
             {
                 title:
                     "HIV Alliance and Shelter Care: Data collection and storage (combined challenge)",
                 description:
                     "It would help us be more efficient if we could use tablets to collect data that could then be imported into a database or spreadsheet. We currently collect the data using paper forms...",
-                video:
-                    "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Ftechoregonswv%2Fvideos%2F303629326982841%2F&show_text=0&width=560"
-            },
-            {
-                title:
-                    "Oregon Family Support Network: Schedule respite child care",
-                description:
-                    "We need a way for parents to schedule respite child care using their phone.",
-                video: ""
+                video: [
+                    "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Ftechoregonswv%2Fvideos%2F303629326982841%2F&show_text=0&width=560",
+                    "https://www.youtube.com/embed/GoVUX02RVpg"
+                ]
             },
             {
                 title:
@@ -70,7 +77,7 @@ export default {
                 title: "City of Eugene: Tools for Community Engagement",
                 description:
                     "Explore using beacon technology to send a signal to a City of Eugene project phone application at project sites. Upon passing a project site, the application would show a notification on the app. Upon opening the app, the community member is presented with the project information and can learn more about it.",
-                video: ""
+                video: "https://www.youtube.com/embed/bb1Rld-Vsl0"
             },
             {
                 title: "City of Eugene - Parking Services: Urban Pong",
@@ -87,9 +94,11 @@ export default {
                 video: "https://www.youtube.com/embed/1DGw25klxrw"
             },
             {
-                title: "Lane County Government: Speed Up America",
-                description:
-                    "Accurate information on the availability of Internet Service Providers, actual prices paid, and real speeds received.",
+                title:
+                    "Lane County Government: Speed Up America (Grand Challenge)",
+                description: `All are welcome to participate. This is a non-competitive challenge where professionals are doing paired programming with students.
+                    This effort uses a single repository and the prize is accomplishment and recognition. The challenge focuses on accurate information on the 
+                    availability of Internet Service Providers, actual prices paid, and real speeds received.`,
                 video:
                     "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Ftechoregonswv%2Fvideos%2F2283236118613879%2F&show_text=0&width=560"
             }
