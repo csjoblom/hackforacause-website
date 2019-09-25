1c1ba541685e420db85c9a7c33e0f93aa15f2881
Kourchenko <dkourchenko@gmail.com>
2019-09-24 21:08:45 -0700

    Fixed Facebook link in Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="py-5 text-center bg-dark">
     <div class="h4c-footer pt-5 pb-3">
       <div id="h4c-twitter" class="h4c-social d-inline mr-5"><a href="https://twitter.com/hashtag/hack4cause?src=hash" target="_blank"><i class="fa fa-twitter fa-3" aria-hidden="true"/></a></div>
-      <div id="h4c-fb" class="h4c-social d-inline mr-5"><a href="https://www.facebook.com/events/240524426488909/" target="_blank"><i class="fa fa-facebook fa-3" aria-hidden="true"/></a></div>
+      <div id="h4c-fb" class="h4c-social d-inline mr-5"><a href="https://www.facebook.com/techoregonswv/" target="_blank"><i class="fa fa-facebook fa-3" aria-hidden="true"/></a></div>
       <div id="h4c-insta" class="h4c-social d-inline mr-5"><a href="https://www.instagram.com/explore/tags/hack4cause/?hl=en" target="_blank"><i class="fa fa-instagram fa-3" aria-hidden="true"/></a></div>
       <div id="h4c-email" class="h4c-social d-inline"><a href="mailto:matt.sayre@techoregon.org"><i class="fa fa-envelope fa-2" aria-hidden="true"/></a></div>
     </div>
